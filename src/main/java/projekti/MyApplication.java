73f86a24c3b2966257c85c22d3afb034943ec152
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-05-05 10:07:34 +0300

    Time zone configured

--- src/main/java/projekti/MyApplication.java
+++ src/main/java/projekti/MyApplication.java
@@ -1,10 +1,17 @@
 package projekti;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC+2"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class);
